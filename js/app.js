9ca1fcf72d3a8fd74ecf7c00b94627d0b7ae9fe7
Sparklebunny Stevens <jeribeth@gmail.com>
2015-06-11 23:35:52 -0600

    Updated footer on non-homepages, playing around with web accessible hire me form, removed About page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-$("[name='my-checkbox']").bootstrapSwitch();
+$Document.Ready(function) {
+
+	$("[name='my-checkbox']").bootstrapSwitch();
+
+
+function elementSupportsAttribute(element,attribute) {
+ var testEl = document.createElement(element);
+ testEl.setAttribute('type',attribute);
+ if (testEl.type=="text") {
+  return false;
+ } else {
+  return true;
+ }
+}
+
+if (!elementSupportsAttribute('input','tel')) {
+// Write custom telephone number validation here.
+}
+if (!elementSupportsAttribute('input','range')) {
+// JavaScript for custom slider/range control goes here.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
